b8049d4122fd886e6655bfb777b6fad712b4abc1
Andrew Head <head.andrewm@gmail.com>
2014-12-25 21:05:36 -0500

    Code: Pondro has memory and talks interpolated speech!

--- pondro/pondro.ino
+++ pondro/pondro.ino
@@ -6,10 +6,20 @@
  by Andrew Head
  */
 
+#include <EEPROM.h>
+
 // Pin constants
 const int soundPin = A0;  // sound detector 'envelope' pin
 const int buttonPin = 4;  // toggle switch
 const int buzzerPin = 2;  // piezo buzzer
+
+// EEPROM constants
+const int shortWriteAddr = 0;
+const int longWriteAddr = 1;
+const int shortCountAddr = 2;
+const int longCountAddr = 3;
+const int longResponseAddr = 4;     // 500 bytes reserved for long responses
+const int shortResponseAddr = 504;  // 500 bytes reserved for short responses
 
 // State constants
 enum convState {
@@ -23,16 +33,101 @@ const int TALK_AMP = 15;          // Minimum sample amplitude to be considered s
 const int TALK_SAMPLES = 3;       // Minimum number of samples above threshold to be considered talking
 const int SILENCE_SAMPLES = 20;   // Number of samples of silence after talking heard to be considered silence
 const int SHORT_RESPONSE_LEN = 20;
-const char boops[] = {20, 17, 5, 4, 3, 18, 21, 25, 2, 1, 25, 26, 21, 20, 18, 23, 5, 2, 1, 1};
+const int LONG_RESPONSE_LEN = 100;
+const int MIN_RESPONSE_LEN = 5;
+const int STORE_RATE = 4;         // 1 / (this number) = chance of storing term
+const int SHORT_RESPONSES = 25;
+const int LONG_RESPONSES = 5;
+const int SAMPLE_DELAY = 100;
+
 
 // Global variables
 int buttonState = 0;
 int amp = 0;
 int silences = 0;
 int talks = 0;
-int responseIndex = 0;
+int responseTime = 0;
 convState state;
+char *response;
+char recording[LONG_RESPONSE_LEN];
+int recordIndex = 0;
 
+char shortResponses[SHORT_RESPONSES][SHORT_RESPONSE_LEN];
+char longResponses[LONG_RESPONSES][LONG_RESPONSE_LEN];
+int shortWriteIndex;
+int longWriteIndex;
+int shortWrites;
+int longWrites;
+
+
+void resetRecording() {
+  for (int i = 0; i < LONG_RESPONSE_LEN; i++) {
+    recording[i] = '\0';
+  }
+  recordIndex = 0;
+}
+
+void recordSample() {
+  recording[recordIndex] = amp;
+  // Recording will wrap around if it gets too long
+  recordIndex = (recordIndex + 1) % LONG_RESPONSE_LEN;
+}
+
+void stopRecording() {
+  // Blank out the samples recorded in the silent periods
+  for (int i = recordIndex; i > recordIndex - SILENCE_SAMPLES; i--) {
+    recording[i] = '\0';
+  }
+}
+
+void storeRecording() {
+  Serial.println("Storing");
+  if (strlen(recording) <= SHORT_RESPONSE_LEN) {
+    for (int i = 0; i < SHORT_RESPONSE_LEN; i++) {
+      shortResponses[shortWriteIndex][i] = recording[i]; 
+    }
+    saveResponse(recording, shortResponseAddr + (shortWriteIndex * SHORT_RESPONSE_LEN), SHORT_RESPONSE_LEN);
+    shortWriteIndex = (shortWriteIndex + 1) % SHORT_RESPONSES;
+    shortWrites = min(shortWrites + 1, SHORT_RESPONSES);
+  }
+  else {
+    for (int i = 0; i < LONG_RESPONSE_LEN; i++) {
+      longResponses[longWriteIndex][i] = recording[i]; 
+    }
+    saveResponse(recording, longResponseAddr + (longWriteIndex * LONG_RESPONSE_LEN), LONG_RESPONSE_LEN);
+    longWriteIndex = (longWriteIndex + 1) % LONG_RESPONSES;
+    longWrites = min(longWrites + 1, LONG_RESPONSES);
+  }
+  saveState();
+}
+
+void processRecording() {
+  // Decide whether to save the recording to Pondro's memory banks
+  if (strlen(recording) < MIN_RESPONSE_LEN) {
+    return; 
+  }
+  else {
+    // Store first two recordings of both lengths
+    if ((strlen(recording) > SHORT_RESPONSE_LEN && longWrites < 2) ||
+        (strlen(recording) > MIN_RESPONSE_LEN && shortWrites < 2)) {
+      storeRecording();
+    }
+    // After that, only store recordings randomly
+    else if (random(STORE_RATE) == 0) {
+      storeRecording();
+    }
+  }
+}
+
+void pickResponse() {
+  // Respond to long requests with long responses
+  // Choose randomly from available phrases
+  if (strlen(recording) > SHORT_RESPONSE_LEN) {
+    response = longResponses[random(longWrites)];
+  } else {
+    response = shortResponses[random(shortWrites)];    
+  }
+}
 
 void wait() {
   noTone(buzzerPin);
@@ -61,31 +156,81 @@ void loop() {
         state = listening;
         silences = 0;
         talks = 1;
+        recordSample();
      }
+    delay(SAMPLE_DELAY);
   }
   else if (state == listening) {
      Serial.println("Listening");
+     recordSample();
      silences = (amp >= TALK_AMP) ? 0 : (silences + 1);
      talks = (amp >= TALK_AMP) ? (talks + 1) : talks;
      if (silences > SILENCE_SAMPLES) {
+       stopRecording();
+       processRecording();
+       pickResponse();
+       responseTime = 0;
        state = (talks >= TALK_SAMPLES) ? responding : waiting;
-     }
+     }  
+    delay(SAMPLE_DELAY);
   }
   else if (state == responding) {
     Serial.println("Responding");
-    tone(buzzerPin, (boops[responseIndex] / 256.0) * 8000);
-    responseIndex++;
+    int responseIndex = responseTime / SAMPLE_DELAY;
+    char boopBefore = response[responseIndex];
+    char boopAfter = response[responseIndex + 1];
+    int boop = (boopBefore + ((responseTime % SAMPLE_DELAY) / (float) SAMPLE_DELAY) * (boopAfter - boopBefore));
+    int freq = 400 + (boop / 256.0) * 3000;
     // Stop response either if it's over or if Pondro has been interrupted
-    if ((responseIndex >= SHORT_RESPONSE_LEN) || amp >= TALK_AMP) {
+    if (boopBefore == '\0' || responseIndex == LONG_RESPONSE_LEN - 2 || amp >= TALK_AMP) {
       state = waiting;
-      responseIndex = 0;
+      responseTime = 0;
+      resetRecording();
+    }
+    if (boop > TALK_AMP) {
+      tone(buzzerPin, freq);
+    } else {
+      noTone(buzzerPin); 
+    }
+    responseTime += 10;
+    delay(10);
+  }
+}
+
+int saveResponse(char* response, int addr, int length) {
+  for (int i = 0; i < length; i++) {
+    EEPROM.write(addr + i, response[i]);   
+  }
+  return 0;
+}
+
+void saveState() {
+  EEPROM.write(shortWriteAddr, shortWriteIndex);
+  EEPROM.write(shortCountAddr, shortWrites);
+  EEPROM.write(longWriteAddr, longWriteIndex);
+  EEPROM.write(longCountAddr, longWrites);
+}
+
+void loadState() {
+  shortWriteIndex = EEPROM.read(shortWriteAddr) != 255 ? EEPROM.read(shortWriteAddr) : 0;
+  longWriteIndex = EEPROM.read(longWriteAddr) != 255 ? EEPROM.read(longWriteAddr) : 0;
+  shortWrites = EEPROM.read(shortCountAddr) != 255 ? EEPROM.read(shortCountAddr) : 0;
+  longWrites = EEPROM.read(longCountAddr) != 255 ? EEPROM.read(longCountAddr) : 0;
+  for (int i = 0; i < shortWrites; i++) {
+    for (int j = 0; j < SHORT_RESPONSE_LEN; j++) {
+      shortResponses[i][j] = EEPROM.read(shortResponseAddr + (i * SHORT_RESPONSE_LEN) + j);
     }
   }
-  
-  delay(100);
+  for (int i = 0; i < longWrites; i++) {
+    for (int j = 0; j < LONG_RESPONSE_LEN; j++) {
+      longResponses[i][j] = EEPROM.read(longResponseAddr + (i * LONG_RESPONSE_LEN) + j);
+    }
+  }
 }
 
 void setup() {
   Serial.begin(9600); 
+  loadState();
+  resetRecording();
   state = waiting;
 }
